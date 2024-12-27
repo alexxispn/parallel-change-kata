52c130fb4789bea36cc77999734b0f6f73d087d8
Fernando Andrés García Hernández <f.garcia.ext@docline.com>
2024-12-27 16:00:07 +0000

    chore: add another case because why not

--- src/Field/ShoppingCart.ts
+++ src/Field/ShoppingCart.ts
@@ -1,13 +1,21 @@
 export class ShoppingCart {
-  prices: number[] = []
+  products: { price: number; quantity: number }[] = []
 
-  add = (price: number) => {
-    this.prices.push(price)
+  add = (price: number, quantity = 1) => {
+    this.products.push({
+      price,
+      quantity,
+    })
   }
 
-  calculateTotalPrice = () => this.prices.reduce((acc, curr) => acc + curr, 0)
+  calculateTotalPrice = () =>
+    this.products.reduce(
+      (price, product) => price + product.price * product.quantity,
+      0,
+    )
 
-  numberOfProducts = () => this.prices.length
+  numberOfProducts = () =>
+    this.products.reduce((quantity, product) => quantity + product.quantity, 0)
 
   hasDiscount = () => this.calculateTotalPrice() > 100
 }
--- test/ShoppingCartShould.test.ts
+++ test/ShoppingCartShould.test.ts
@@ -1,33 +1,43 @@
-import { ShoppingCart } from "../src/Field/ShoppingCart.js";
-import { describe, it, expect, beforeEach } from "vitest";
+import { ShoppingCart } from "../src/Field/ShoppingCart.js"
+import { describe, it, expect, beforeEach } from "vitest"
 
 describe("shopping cart", () => {
-  let cart: ShoppingCart;
+  let cart: ShoppingCart
   beforeEach(() => {
-    (cart = new ShoppingCart());
-  });
+    cart = new ShoppingCart()
+  })
 
   it("calculates the final price", () => {
-    cart.add(10);
+    cart.add(10)
 
-    expect(cart.calculateTotalPrice()).toBe(10);
-  });
+    expect(cart.calculateTotalPrice()).toBe(10)
+  })
 
   it("knows the number of items", () => {
-    cart.add(10);
+    cart.add(10)
 
-    expect(cart.numberOfProducts()).toBe(1);
-  });
+    expect(cart.numberOfProducts()).toBe(1)
+  })
 
   it("may offer discounts when there is at least one expensive product", () => {
-    cart.add(120);
+    cart.add(120)
 
-    expect(cart.hasDiscount()).toBeTruthy();
-  });
+    expect(cart.hasDiscount()).toBeTruthy()
+  })
 
   it("does not offer discount for cheap products", () => {
-    cart.add(10);
+    cart.add(10)
 
-    expect(cart.hasDiscount()).toBeFalsy();
-  });
-});
+    expect(cart.hasDiscount()).toBeFalsy()
+  })
+
+  it("allows adding more than one item at the same time and checks quantities", () => {
+    cart.add(10, 2)
+    expect(cart.numberOfProducts()).toBe(2)
+  })
+
+  it("allows adding more than one item at the same time and checks prices", () => {
+    cart.add(10, 2)
+    expect(cart.calculateTotalPrice()).toBe(20)
+  })
+})
